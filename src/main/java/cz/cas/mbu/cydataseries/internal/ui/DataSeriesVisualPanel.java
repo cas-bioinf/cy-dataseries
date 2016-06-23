fa68d178c5426f9c61376ebc5d95b83ed5ff84dc
martin_cerny <cerny.m@gmail.com>
2016-06-23 15:51:52 +0200

    Display visual TS for a selected edge

--- src/main/java/cz/cas/mbu/cydataseries/internal/ui/DataSeriesVisualPanel.java
+++ src/main/java/cz/cas/mbu/cydataseries/internal/ui/DataSeriesVisualPanel.java
@@ -5,39 +5,27 @@ import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import javax.swing.Icon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.application.swing.CytoPanelComponent2;
 import org.cytoscape.application.swing.CytoPanelName;
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyIdentifiable;
 import org.cytoscape.model.CyNetwork;
-import org.cytoscape.model.CyNetworkManager;
 import org.cytoscape.model.CyNode;
 import org.cytoscape.model.CyRow;
 import org.cytoscape.model.CyTableUtil;
 import org.cytoscape.model.events.RowsSetEvent;
 import org.cytoscape.model.events.RowsSetListener;
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.xy.DefaultXYDataset;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.util.ArrayUtilities;
 
-import cz.cas.mbu.cydataseries.DataSeries;
 import cz.cas.mbu.cydataseries.DataSeriesManager;
 import cz.cas.mbu.cydataseries.DataSeriesMappingManager;
-import cz.cas.mbu.cydataseries.DataSeriesStorageProvider;
 import cz.cas.mbu.cydataseries.TimeSeries;
 import cz.cas.mbu.cydataseries.internal.TimeSeriesChartContainer;
 
@@ -98,6 +86,25 @@ public class DataSeriesVisualPanel extends JPanel implements CytoPanelComponent2
 	public String getIdentifier() {
 		return "cz.cas.mbu.cydataseries.dataSeriesVisual";
 	}
+	
+	private void updateChartsWithRow(Class<? extends CyIdentifiable> targetClass, CyRow row)
+	{
+		List<TimeSeries> allSeries = new ArrayList<>();
+		List<Integer> rowIds = new ArrayList<>();
+		
+		dataSeriesMappingManager.getAllMappings(targetClass, TimeSeries.class).entrySet().forEach(
+				(entry) -> {
+					Integer id = row.get(entry.getKey(), DataSeriesMappingManager.MAPPING_COLUMN_CLASS);
+					if(id != null)
+					{
+						allSeries.add(entry.getValue());
+						rowIds.add(id);							
+					}
+				});
+		
+		chartContainer.setSeriesData(allSeries, rowIds);			
+		
+	}
 
 	private void updateCharts()
 	{
@@ -107,24 +114,20 @@ public class DataSeriesVisualPanel extends JPanel implements CytoPanelComponent2
 		{
 			CyNode node = nodes.get(0);
 			CyRow row = network.getRow(node);
-			List<TimeSeries> allSeries = new ArrayList<>();
-			List<Integer> rowIds = new ArrayList<>();
-			
-			dataSeriesMappingManager.getAllMappings(CyNode.class, TimeSeries.class).entrySet().forEach(
-					(entry) -> {
-						Integer id = row.get(entry.getKey(), DataSeriesMappingManager.MAPPING_COLUMN_CLASS);
-						if(id != null)
-						{
-							allSeries.add(entry.getValue());
-							rowIds.add(id);							
-						}
-					});
-			
-			chartContainer.setSeriesData(allSeries, rowIds);			
+			updateChartsWithRow(CyNode.class, row);			
 		}
 		else
 		{
-			chartContainer.setSeriesData(Collections.EMPTY_LIST, Collections.EMPTY_LIST);			
+			List<CyEdge> edges = CyTableUtil.getEdgesInState(network, "selected", true);
+			if(!edges.isEmpty())
+			{
+				CyRow row = network.getRow(edges.get(0));
+				updateChartsWithRow(CyEdge.class, row);				
+			}
+			else
+			{
+				chartContainer.setSeriesData(Collections.EMPTY_LIST, Collections.EMPTY_LIST);
+			}
 		}
 	}
 	
@@ -132,7 +135,8 @@ public class DataSeriesVisualPanel extends JPanel implements CytoPanelComponent2
 	public void handleEvent(RowsSetEvent e) {
 		if (!e.containsColumn(CyNetwork.SELECTED) || ignoreSelection)
 			return;
-		if(e.getSource().equals(cyApplicationManager.getCurrentNetwork().getTable(CyNode.class, CyNetwork.LOCAL_ATTRS)) )
+		if(e.getSource().equals(cyApplicationManager.getCurrentNetwork().getTable(CyNode.class, CyNetwork.LOCAL_ATTRS)) 
+				|| e.getSource().equals(cyApplicationManager.getCurrentNetwork().getTable(CyEdge.class, CyNetwork.LOCAL_ATTRS)))
 		{
 			updateCharts();
 	
