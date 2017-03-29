d93c72aa66606850084f9f73fe149e4a1a31e9a6
craigfaria <craigfaria7@gmail.com>
2017-03-29 20:34:51 +0530

    Resetting changes made to ExportDataSeriesTask

--- src/main/java/cz/cas/mbu/cydataseries/internal/tasks/ExportDataSeriesTask.java
+++ src/main/java/cz/cas/mbu/cydataseries/internal/tasks/ExportDataSeriesTask.java
@@ -24,24 +24,24 @@ public class ExportDataSeriesTask extends AbstractValidatedTask {
 
 	@Tunable(description = "Series to export", required = true)
 	public ListSingleSelection<DataSeries<?, ?>> dataSeries;
-
-	@Tunable(description = "Output file", required = true, params = "input=false;fileCategory=table")
+	
+	@Tunable(description="Output file",required = true, params="input=false;fileCategory=table")
 	public File outputFile;
-
+	
 	private final DataSeriesStorageManager dataSeriesStorageManager;
-
-	public ExportDataSeriesTask(DataSeriesManager dataSeriesManager,
-			DataSeriesStorageManager dataSeriesStorageManager) {
-		dataSeries = new ListSingleSelection<>(
-				dataSeriesManager.getAllDataSeries());
+	
+	public ExportDataSeriesTask(DataSeriesManager dataSeriesManager, DataSeriesStorageManager dataSeriesStorageManager) {
+		dataSeries = new ListSingleSelection<>(dataSeriesManager.getAllDataSeries());
 		this.dataSeriesStorageManager = dataSeriesStorageManager;
 	}
-
+	
 	@ProvidesTitle
-	public String getTitle() {
+	public String getTitle()
+	{
 		return "Export Data series";
 	}
-
+	
+	
 	@Override
 	protected ValidationState getValidationState(StringBuilder messageBuilder) {
 		if (outputFile == null || outputFile.isDirectory()) {
@@ -49,13 +49,11 @@ public class ExportDataSeriesTask extends AbstractValidatedTask {
 			return ValidationState.INVALID;
 		}
 		if (!outputFile.getParentFile().exists()) {
-			messageBuilder
-					.append("The parent directory of the output file has to exist.");
-			return ValidationState.INVALID;
+			messageBuilder.append("The parent directory of the output file has to exist.");
+			return ValidationState.INVALID;			
 		}
 		if (outputFile.exists()) {
-			messageBuilder.append("Are you sure you want to overwrite file '"
-					+ outputFile.toString() + "'?");
+			messageBuilder.append("Are you sure you want to overwrite file '" + outputFile.toString() + "'?");
 			return ValidationState.REQUEST_CONFIRMATION;
 		}
 		return ValidationState.OK;
@@ -63,17 +61,12 @@ public class ExportDataSeriesTask extends AbstractValidatedTask {
 
 	@Override
 	public void run(TaskMonitor taskMonitor) throws Exception {
-
 		DataSeries<?, ?> selectedSeries = dataSeries.getSelectedValue();
-
-		DataSeriesStorageProvider storageProvider = dataSeriesStorageManager
-				.getStorageProvider(selectedSeries.getClass());
-		if (storageProvider == null) {
-			throw new DataSeriesException(
-					"Could not export - no storage provider found for class "
-							+ selectedSeries.getClass().getName());
+		DataSeriesStorageProvider storageProvider = dataSeriesStorageManager.getStorageProvider(selectedSeries.getClass());
+		if(storageProvider == null) {
+			throw new DataSeriesException("Could not export - no storage provider found for class " + selectedSeries.getClass().getName());
 		} else {
-			storageProvider.saveDataSeries(selectedSeries, outputFile);
+			storageProvider.saveDataSeries(selectedSeries, outputFile);			
 		}
 	}
 
